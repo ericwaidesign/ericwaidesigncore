f0b5f1e5c45dc8103bdc97953e3caf4042353b76
ericwaidesign@gmail.com <ericwaidesign@gmail.com>
2018-07-27 23:23:21 -0400

    added images page

--- server/routes/general.js
+++ server/routes/general.js
@@ -24,4 +24,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, '../../client/build/index.html'));
     });
 
+    app.get('/images', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/build/images.html'));
+    });
+
 }
