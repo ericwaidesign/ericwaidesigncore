023babaa98db2031423e9a26ac9f76459c9cd474
ericwaidesign@gmail.com <ericwaidesign@gmail.com>
2018-07-28 17:08:54 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  // Initialize state
-  state = { images: [] }
-
-  // Fetch images after first mount
-  componentDidMount() {
-    // this.getImages();
-  }
-
-  getPasswords = () => {
-    // Get the i and store them in state
-    fetch('/api/images')
-      .then(res => res.json())
-      .then(images => this.setState({ images }));
-  }
 
   render() {
     return (
       <div className="App">
         {/* TESTING */}
-        ...
+        <div className="default center"> ¯\_(ツ)_/¯ </div>
       </div>
     );
   }
