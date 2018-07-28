a383a44b1a4371ef617fd36d37448b6a1cc01278
ericwaidesign@gmail.com <ericwaidesign@gmail.com>
2018-07-28 17:51:40 -0400

    updates

--- server/routes/general.js
+++ server/routes/general.js
@@ -18,9 +18,9 @@ module.exports = function(app) {
         res.send({ express: 'Hello From Express' });
     });
     
-    app.get('/api/images', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/build/images.html'));
-    });
+    // app.get('/api/images', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../../client/build/images.html'));
+    // });
     
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
