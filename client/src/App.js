58886a9339e1d6e51fbf11b68ab0c5a3f64fb87f
ericwaidesign@gmail.com <ericwaidesign@gmail.com>
2018-07-30 23:33:50 -0400

    added image loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ImageLoad from "./components/ImageLoad/ImageLoad";
 import '../src/assets/css/App.css';
 
 class App extends Component {
@@ -24,8 +25,12 @@ class App extends Component {
 
   render() {
     const imageArray = this.state.images.map((image) => {
+      var highRes = require('../src/assets/images/' + image);
+      var lowRes = require('../src/assets/lowRes/lowRes_' + image);
       return (
-        <img src={require('../src/assets/images/' + image)} />
+        <ImageLoad placeholder={lowRes} src={highRes} />
+
+        // <img src={require('../src/assets/images/' + image)} />
       );
     });
 
