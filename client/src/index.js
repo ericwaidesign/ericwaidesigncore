fd64c6962bd764366a8147bf86186dca012a3c98
ericwaidesign <ericwaidesign@gmail.com>
2019-03-23 01:19:56 -0400

    revised index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
-import App from './components/ImageLoader/ImageLoaderApp';
+import ImageLoaderApp from './components/ImageLoader/ImageLoaderApp';
+import registerServiceWorker from './registerServiceWorker';
 
 /* Render ImageLoader component into DOM */
-ReactDOM.render(<App />, document.getElementById('SimpleImageSliderRoot'));
+ReactDOM.render(<ImageLoaderApp />, document.getElementById('ImageLoaderRoot'));
+
+// cache assets for offline or slow network
+registerServiceWorker();
