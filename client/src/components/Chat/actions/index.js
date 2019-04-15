53d51d1500196df364fb63462cd5ec9698fa3f5a
ericwaidesign <ericwaidesign@gmail.com>
2019-04-15 15:42:43 +0000

    updates

--- client/src/components/Chat/actions/index.js
+++ client/src/components/Chat/actions/index.js
@@ -1,25 +1,30 @@
 "strict mode"
 
+/**
+ * @description create action which contains payloads of information that
+ * send data from the application to the store. These are the only 
+ * information send to the store.
+ */
 import * as types from '../constants/ActionTypes';
 
 let nextMessageId = 0;
 const nextUserId = 0;
 
 export const addMessage = (message, author) => ({
-  type: types.ADD_MESSAGE,
-  id: nextMessageId++,
-  message,
-  author
+    type: types.ADD_MESSAGE,
+    id: nextMessageId++,
+    message,
+    author
 });
 
 export const messageReceived = (message, author) => ({
-  type: types.MESSAGE_RECEIVED,
-  id: nextMessageId++,
-  message,
-  author
+    type: types.MESSAGE_RECEIVED,
+    id: nextMessageId++,
+    message,
+    author
 });
 
 export const populateUsersList = users => ({
-  type: types.USERS_LIST,
-  users
+    type: types.USERS_LIST,
+    users
 });
