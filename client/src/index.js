275559a921392eb662ef4499022dce36e9d689bd
Eric Wai <ericwaidesign@gmail.com>
2019-04-14 21:22:27 -0400

    added actions

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,31 @@
 /**
- * ericwaidesigncore\client\src\index.js
  * @author <ericwaidesign@gmail.com>
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import ImageLoaderApp from './components/ImageLoader/App';
 import ChatApp from './components/Chat/App';
 
+import chat from './reducers';
+
 import './assets/css/index.css';
+
+const store = createStore(chat);
 
 /* Render ImageLoader component into DOM */
 ReactDOM.render(<ImageLoaderApp />, document.getElementById('ImageLoaderRoot'));
 
-/* Render ChatUI component into DOM */
+/* Render Chat component into DOM */
 ReactDOM.render(
-    
-    <ChatApp />, document.getElementById('ChatRoot')
+    // indead of telling ReactDOM to render <ChatApp />, Provider makes
+    //  the store available to all the components of the ChatApp, without
+    //  explicitly passing it down.
+    <Provider store={store}>
+        <ChatApp />
+    </Provider>,
+    document.getElementById('ChatRoot')
 );
