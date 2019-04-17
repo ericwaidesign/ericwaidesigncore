76e791171a2c919d93668054a3ea27b3992e8afd
ericwaidesign <ericwaidesign@gmail.com>
2019-04-17 18:16:58 +0000

    update

--- server/configs/envs/config.dev.js
+++ server/configs/envs/config.dev.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	// secret password to be used for the verification signature section of the JWT
-	PORT: process.env.PORT || 9000,
+	PORT: process.env.PORT || 80,
 	DB: {
         DEBUG: true,
         MONGODB: "postdb1",
