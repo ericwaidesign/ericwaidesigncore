973c4d402b55c3c8220d3ea2af0ffbd7f43d637c
ericwaidesign <ericwaidesign@gmail.com>
2019-04-19 19:20:41 +0000

    added Chat assets, updated index.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ class Chat extends Component {
 
         const addMessage = data => {
             console.log(data);
-            this.setState({messages: [...this.state.message, data]});
+            this.setState({ messages: [...this.state.message, data] });
             console.log(this.state.messages);
         }
 
@@ -31,7 +31,7 @@ class Chat extends Component {
                 author: this.state.username,
                 message: this.state.messsge
             });
-            this.setState({message: ""});
+            this.setState({ message: "" });
         }
     }
 
@@ -42,7 +42,22 @@ class Chat extends Component {
                     <div className="col-4">
                         <div className="card">
                             <div className="card-body">
-
+                                <div className="card-title">Global Chat</div>
+                                <hr />
+                                <div className="messages">
+                                    {this.state.messages.map(message => {
+                                        return (
+                                            <div>{message.author}: {message.message}</div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({ username: ev.target.value })} className="form-control" />
+                                <br />
+                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({ message: ev.target.value })} />
+                                <br />
+                                <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
                             </div>
                         </div>
                     </div>
