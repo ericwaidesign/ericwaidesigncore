78fc47fd3f185c732ec6efc09c9e1b718ee42401
ericwaidesign <ericwaidesign@gmail.com>
2019-04-23 18:31:39 +0000

    Updated

--- client/src/components/Chat/actions/index.js
+++ client/src/components/Chat/actions/index.js
@@ -1,23 +1,41 @@
-import * as types from '../constants/ActionTypes'
+/**
+ * @description Actions is a regular JavaScript object that usually
+ * has 2 properties (type and payload).
+ */
+import * as types from '../constants/ActionTypes';
 
-let nextMessageId = 0
-const nextUserId = 0
+let nextMessageId = 0;
+const nextUserId = 0;
 
+/**
+ * @description
+ * @param (message) - The message sent from client.
+ * @param (author) - The sender(client) of the message.
+ */
 export const addMessage = (message, author) => ({
-  type: types.ADD_MESSAGE,
-  id: nextMessageId++,
-  message,
-  author
-})
+    type: types.ADD_MESSAGE,
+    id: nextMessageId++,
+    message,
+    author
+});
 
+/**
+ * @description
+ * @param (message) - The message sent from client.
+ * @param (author) - The sender(client) of the message.
+ */
 export const messageReceived = (message, author) => ({
-  type: types.MESSAGE_RECEIVED,
-  id: nextMessageId++,
-  message,
-  author
-})
+    type: types.MESSAGE_RECEIVED,
+    id: nextMessageId++,
+    message,
+    author
+});
 
+/** @description
+ * @param (message) - The message sent from client.
+ * @param (author) - The sender(client) of the message.
+ */
 export const populateUsersList = users => ({
-  type: types.USERS_LIST,
-  users
+    type: types.USERS_LIST,
+    users
 })
