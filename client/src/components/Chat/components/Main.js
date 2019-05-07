42fca29800c5525500878102ae19619a9aa572bc
ericwaidesign <ericwaidesign@gmail.com>
2019-05-07 21:11:21 +0000

    added Main.js

---
+++ client/src/components/Chat/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.css";
+
+/**
+ * @description
+ */
+const Main = () => {
+    return <main className = "Main">Main Stuff</main>;
+};
+
+export default Main;
--- client/src/components/Chat/components/Sidebar.js
+++ client/src/components/Chat/components/Sidebar.js
@@ -1,2 +1,12 @@
 import React from "react";
 import "./Sidebar.css";
+
+/**
+ * @description
+ */
+const Sidebar = () => {
+    return <aside className = "Sidebar">Sidebar</aside>
+}
+
+export default Sidebar;
+
