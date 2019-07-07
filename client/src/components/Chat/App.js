e6f2e16ba053d98c72c3be4efc8a7d3fd9635556
ericwaidesign <ericwaidesign@gmail.com>
2019-07-06 21:11:31 -0400

    new chat structure

--- client/src/components/Chat/App.js
+++ client/src/components/Chat/App.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
+import Chat from "./Chat";
 
-class ChatApp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "Enter your message."
-        }
-    }
-
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-
-    handleSubmit(event) {
-        //
-        event.preventDefault();
-    }
-
-    addMessage() {
-
-    } 
-
+class App extends Component {
     render() {
         return (
-            <div id="chatAppContainer">
-                <form onSubmit={this.handleSubmit}>
-                    <textarea id="chatAppMessage" value={this.state.value} onChange={this.handleChange} />
-
-                    <input type="submit" value="Send" />
-                </form>
+            <div>
+                <Chat />
             </div>
         );
     }
 }
 
-export default ChatApp;
+export default App;
---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,76 @@
+import React from "react";
+import io from "socket.io-client";
+
+const event = require('./constants/events');
+
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // holds the current values
+        this.state = {
+            username: '',
+            message: '',
+            messages: [] // holds all the messages(receive and send)
+        };
+
+        this.socket = io('localhost:9000');
+
+        this.socket.on(event.RECEIVE_MESSAGE, function (data) {
+            addMessage(data);
+        });
+
+        const addMessage = data => {
+            console.log(data);
+            this.setState({ messages: [...this.state.messages, data] });
+            console.log(this.state.messages);
+        };
+
+        this.sendMessage = ev => {
+            ev.preventDefault();
+            this.socket.emit(event.SEND_MESSAGE, {
+                author: this.state.username,
+                message: this.state.message
+            })
+            this.setState({ message: '' });
+
+        }
+    }
+    render() {
+        return (
+            <div className="chat-app-container">
+                <div className="chat-app-body">
+                    <div className="card-title">Global Chat</div>
+                    <hr />
+                    <div className="messages">
+                        {this.state.messages.map(message => {
+                            return (
+                                <div>{message.author}: {message.message}</div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className="chat-app-footer">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange={ev => this.setState({ username: ev.target.value })}
+                        className="" />
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Message"
+                        className=""
+                        value={this.state.message}
+                        onChange={ev => this.setState({ message: ev.target.value })} />
+                    <br />
+                    <button onClick={this.sendMessage} className="">Send</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Chat;
