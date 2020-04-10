74ab130d4be4ff3c77a0ce6eb57440dd84feff54
Eric Wai <ericwaidesign@gmail.com>
2020-04-10 16:28:09 -0400

    updated config.prod.sample.js

--- server/configs/envs/config.prod.sample.js
+++ server/configs/envs/config.prod.sample.js
@@ -4,7 +4,7 @@ module.exports = {
 	PORT: process.env.PORT || 9000,
 	DB: {
 		DEBUG: true,
-		MONGODB: "postdb1",
-		HOST: "core:Password1!@ds233531.mlab.com:33531",
+		MONGODB: "",
+		HOST: "",
 	}
 };
