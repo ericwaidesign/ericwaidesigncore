4d2945fe8ec7e55bbda44072218bc1f0762a2a96
Eric Wai <ericwaidesign@gmail.com>
2020-04-24 22:04:05 -0400

    update config

---
+++ server/configs/envs/config.dev.js
@@ -0,0 +1,10 @@
+"use strict"
+
+module.exports = {
+        PORT: process.env.PORT || 9000,
+        DB: {
+                DEBUG: true,
+                MONGODB: "postdb1",
+                HOST: "core:Password1!@ds233531.mlab.com:33531",
+        }
+};
